afc1abcaa7c6fe9b6d52c12bd2004483dff62c29
AndreBtt <bittencourtandre@hotmail.com>
2017-10-10 10:28:46 -0300

    Agendar horario layout criado

---
+++ app/src/main/java/com/bittencourt/andre/computsal/AgendarHorario.java
@@ -0,0 +1,13 @@
+package com.bittencourt.andre.computsal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendarHorario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_agendar_horario);
+    }
+}
